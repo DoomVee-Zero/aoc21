fa21277d859b9d9adba7923a624809f570a49651
Roman Shchekotov <rom.shchekotov@gmail.com>
2021-12-06 00:05:26 +0100

    feat: enhance boilerplate ✨
    Signed-off-by: Roman Shchekotov <rom.shchekotov@gmail.com>
    
    Took 3 minutes

--- test/main.spec.ts
+++ test/main.spec.ts
@@ -2,14 +2,15 @@ import { alpha, beta } from "@/main";
 import * as fs from "fs";
 
 let aocInput;
+let sampleInput;
 beforeAll(() => {
     aocInput = fs.readFileSync('res/input.txt', { encoding: 'utf-8' });
+    sampleInput = fs.readFileSync('res/sample.txt', { encoding: 'utf-8' });
 });
 
 describe('Part One of Day X', () => {
     it('should be Y', () => {
-        let input = ``;
-        expect(alpha(input)).toBe(null);
+        expect(alpha(sampleInput)).toBe(null);
     });
     test('aoc input', () => {
         expect(alpha(aocInput)).toBe(null);
@@ -18,8 +19,7 @@ describe('Part One of Day X', () => {
 
 describe('Part Two of Day X', () => {
     it('should be Z', () => {
-        let input = ``;
-        expect(beta(input)).toBe(null);
+        expect(beta(sampleInput)).toBe(null);
     });
 
     test('aoc input', () => {
