bc751bc06e8efc14c908a119b49d5d8cfe975332
Roman Shchekotov <rom.shchekotov@gmail.com>
2021-12-06 00:02:28 +0100

    feat: part I and II for day 4 ✨ ✅
    Signed-off-by: Roman Shchekotov <rom.shchekotov@gmail.com>
    
    Took 1 hour 27 minutes

--- src/main.ts
+++ src/main.ts
@@ -1,11 +1,92 @@
+type BingoBoard = Array<Array<number>>;
+
+interface Instructions {
+    order: number[];
+    boards: BingoBoard[];
+}
+
 export function alpha(input: string) {
-    return;
+    let instructions = processInput(input);
+    let order = instructions.order;
+    let boards = instructions.boards;
+    let boardIndex = -1;
+
+    let move = order.shift();
+    while(boardIndex === -1 && move !== undefined) {
+        boards = boards.map(board => markMove(board, move!));
+        if(boards.every(board => !hasWon(board))) {
+            move = order.shift();
+        } else {
+            boardIndex = boards.findIndex((board) => hasWon(board));
+        }
+    }
+    return computeScore(boards[boardIndex], move!);
 }
 
 export function beta(input: string) {
-    return;
+    let instructions = processInput(input);
+    let order = instructions.order;
+    let boards = instructions.boards;
+
+    let move = order.shift();
+    while(move !== undefined) {
+        boards = boards.map(board => markMove(board, move!));
+        if(boards.some(board => !hasWon(board))) {
+            boards = boards.filter(board => !hasWon(board));
+            move = order.shift();
+        } else break;
+    }
+
+    return computeScore(boards[boards.length - 1], move!);
 }
 
-function processInput(input: string) {
-    return;
+/* Utility */
+function computeScore(board: BingoBoard, lastMove: number) {
+    return board.map((row) => {
+        let unmarked = row.filter(n => n !== -1);
+        if (unmarked.length === 0) return 0;
+        return unmarked.reduce((a, c) => a + c);
+    }).reduce((a, c) => a + c) * lastMove;
+}
+
+function markMove(board: BingoBoard, move: number) {
+    let index = board.flat().indexOf(move);
+    if(index !== -1) {
+        let y = Math.floor(index / board.length);
+        let x = index % board.length;
+        board[y][x] = -1;
+    }
+    return board;
+}
+
+function hasWon(board: BingoBoard): boolean {
+    let sum = [0, 0];
+    for(let y = 0; y < board.length; y++) {
+        for (let x = 0; x < board[y].length; x++) {
+            sum[0] += board[y][x];
+            sum[1] += board[x][y];
+        }
+        if(sum[0] == -5 || sum[1] == -5) {
+            return true;
+        } else {
+            sum = [0, 0];
+        }
+    }
+    return false;
+}
+
+/* Input Processing*/
+function parseBoard(input: string) {
+    return input.trim().split(/\r?\n\s*/)
+        .map(line => line.trim().split(/ +/)
+            .map(num => parseInt(num)));
+}
+
+function processInput(input: string): Instructions {
+    let data: Array<string> = input.split(/(?:\r?\n){2}/);
+    let order = data.shift()!.split(/,/).map(num => parseInt(num));
+    let boards = data.map(board => {
+        return parseBoard(board);
+    });
+    return { order, boards };
 }
--- test/main.spec.ts
+++ test/main.spec.ts
@@ -1,28 +1,27 @@
 import { alpha, beta } from "@/main";
 import * as fs from "fs";
 
-let aocInput;
+let aocInput, sampleInput;
 beforeAll(() => {
+    sampleInput = fs.readFileSync('res/sample.txt', { encoding: 'utf-8' });
     aocInput = fs.readFileSync('res/input.txt', { encoding: 'utf-8' });
 });
 
-describe('Part One of Day X', () => {
-    it('should be Y', () => {
-        let input = ``;
-        expect(alpha(input)).toBe(null);
+describe('Part One of Day 4', () => {
+    it('should be 4512', () => {
+        expect(alpha(sampleInput)).toBe(4512);
     });
     test('aoc input', () => {
-        expect(alpha(aocInput)).toBe(null);
+        expect(alpha(aocInput)).toBe(58838);
     });
 });
 
-describe('Part Two of Day X', () => {
-    it('should be Z', () => {
-        let input = ``;
-        expect(beta(input)).toBe(null);
+describe('Part Two of Day 4', () => {
+    it('should be 1924', () => {
+        expect(beta(sampleInput)).toBe(1924);
     });
 
     test('aoc input', () => {
-        expect(beta(aocInput)).toBe(null);
+        expect(beta(aocInput)).toBe(6256);
     });
 });
