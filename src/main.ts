0368a636e82ffe380e29542e5114aa22520a8ab8
Roman Shchekotov <rom.shchekotov@gmail.com>
2021-12-06 11:16:21 +0100

    feat: part I and II for day 6 ✨ ✅
    Signed-off-by: Roman Shchekotov <rom.shchekotov@gmail.com>
    
    Took 34 minutes

--- src/main.ts
+++ src/main.ts
@@ -1,11 +1,59 @@
 export function alpha(input: string) {
-    return;
+    return computeGrowth(input, 80);
 }
 
 export function beta(input: string) {
-    return;
+    return computeSmartGrowth(input, 256);
 }
 
-function processInput(input: string) {
-    return;
+function computeGrowth(input: string, days: number) {
+    let ages = processInput(input);
+    for (let i = 0; i < days; i++) {
+        grow(ages);
+    }
+    return ages.length;
+}
+
+function computeSmartGrowth(input: string, days: number) {
+    let ages = processInput(input);
+    let evol = smartGrow(ages, days);
+    return evol.reduce((a, c) => a + c);
+}
+
+function grow(ages: Array<number>) {
+    let initial = ages.length;
+    for(let i = 0; i < initial; i++) {
+        if(ages[i] === 0) {
+            ages[i] = 6;
+            ages.push(8);
+        } else {
+            ages[i]--;
+        }
+    }
+}
+
+function smartGrow(ages: Array<number>, days: number) {
+    let initial = [0, 0, 0, 0, 0, 0, 0, 0, 0];
+    ages.forEach(age => initial[age]++);
+
+    for(let i = 0; i < days; i++) {
+        let ascendants = initial[0];
+        rotLeft(initial, 0, 6)
+        initial[6] += initial[7];
+        initial[7] = initial[8];
+        initial[8] = ascendants;
+    }
+    return initial;
+}
+
+function rotLeft(array: Array<number>, left: number, right: number) {
+    let off = array[left];
+    for(let i = left + 1; i <= right; i++) {
+        array[i-1] = array[i];
+    }
+    array[right] = off;
+}
+
+function processInput(input: string): Array<number> {
+    return input.split(',').map(text => parseInt(text));
 }
--- test/main.spec.ts
+++ test/main.spec.ts
@@ -1,4 +1,4 @@
-// import { alpha, beta } from "@/main";
+import { alpha, beta } from "@/main";
 import * as fs from "fs";
 
 let aocInput;
@@ -8,22 +8,22 @@ beforeAll(() => {
     sampleInput = fs.readFileSync('res/sample.txt', { encoding: 'utf-8' });
 });
 
-describe('Part One of Day X', () => {
-    // it('should be Y', () => {
-    //     expect(alpha(sampleInput)).toBe(null);
-    // });
-    //
-    // test('aoc input', () => {
-    //     expect(alpha(aocInput)).toBe(null);
-    // });
+describe('Part One of Day 6', () => {
+    it('should be 5934', () => {
+        expect(alpha(sampleInput)).toBe(5934);
+    });
+
+    test('aoc input', () => {
+        expect(alpha(aocInput)).toBe(393019);
+    });
 });
 
-describe('Part Two of Day X', () => {
-    // it('should be Z', () => {
-    //     expect(beta(sampleInput)).toBe(null);
-    // });
-    //
-    // test('aoc input', () => {
-    //     expect(beta(aocInput)).toBe(null);
-    // });
+describe('Part Two of Day 6', () => {
+    it('should be 26984457539', () => {
+        expect(beta(sampleInput)).toBe(26984457539);
+    });
+
+    test('aoc input', () => {
+        expect(beta(aocInput)).toBe(1757714216975);
+    });
 });
